74c2b82b5244dcfb60aeb5af738b0fa812668142
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-08-04 09:58:47 +0430

    Use laravel collective for Form label and text

--- resources/views/slider/create.blade.php
+++ resources/views/slider/create.blade.php
@@ -13,6 +13,9 @@
             </div>
             {!! Form::open(['route' => 'slider.store', 'method' => 'post']) !!}
 
+            {!! Form::label('firstName', 'نام', ['class' => 'control-label','style'=>'color:green;font-size:20px;font-weight:bold']) !!}
+            {!! Form::text('firstName', null, ['class' => 'form-control','palceholder'=>'Please enter your name','style'=>'border:2px inset blue;']) !!}
+
             {!! Form::close() !!}
 
         </section>
