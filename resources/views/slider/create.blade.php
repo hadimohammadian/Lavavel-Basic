f89ce44e502b9bb06d2429ded7e91c62d271f27b
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-08-04 10:06:24 +0430

    Use laravel collective for add Form password item

--- resources/views/slider/create.blade.php
+++ resources/views/slider/create.blade.php
@@ -15,6 +15,8 @@
 
             {!! Form::label('firstName', 'نام', ['class' => 'control-label','style'=>'color:green;font-size:20px;font-weight:bold']) !!}
             {!! Form::text('firstName', null, ['class' => 'form-control','palceholder'=>'Please enter your name','style'=>'border:2px inset blue;']) !!}
+           {!! Form::label('password', 'کلمه عبور', ['class' => 'control-label']) !!}
+            {!! Form::password('password', ['class' => 'form-control']) !!}
 
             {!! Form::close() !!}
 
