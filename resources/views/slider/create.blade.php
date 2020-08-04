34055e6fdf1ceae0eb73461f5b3555d5788e7618
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-08-04 11:27:52 +0430

    Use laravel collective for add Form Email item

--- resources/views/slider/create.blade.php
+++ resources/views/slider/create.blade.php
@@ -15,8 +15,10 @@
 
             {!! Form::label('firstName', 'نام', ['class' => 'control-label','style'=>'color:green;font-size:20px;font-weight:bold']) !!}
             {!! Form::text('firstName', null, ['class' => 'form-control','palceholder'=>'Please enter your name','style'=>'border:2px inset blue;']) !!}
-           {!! Form::label('password', 'کلمه عبور', ['class' => 'control-label']) !!}
+            {!! Form::label('password', 'کلمه عبور', ['class' => 'control-label']) !!}
             {!! Form::password('password', ['class' => 'form-control']) !!}
+            {!! Form::label('email', 'ایمیل', ['class' => 'control-label']) !!}
+            {!! Form::email('email', null, ['class' => 'form-control']) !!}
 
             {!! Form::close() !!}
 
