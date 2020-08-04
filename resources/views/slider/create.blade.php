3cf8ad1cf3a70a1c1e09fe94db35c06309f68e80
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-08-04 09:56:57 +0430

    Use laravel collective for generate Form items

--- resources/views/slider/create.blade.php
+++ resources/views/slider/create.blade.php
@@ -7,45 +7,15 @@
 @section('content')
     <section class="row mt-5">
         <section class="col-8 offset-2  ">
-            <form action="{{route('slider.store')}}" method="post" enctype="multipart/form-data">
-                @csrf
+            <div>
+                {!! ' this is a text from laravel collective '  !!}
 
-                <section class="form-group">
-                    <label for="firstName">First Name</label>
-                    <input type="text" name="firstName" id="firstName" class="form-control">
-                </section>
-                <section class="form-group">
-                    <label for="lastName">Last Name</label>
-                    <input type="text" name="lastName" id="lastName" class="form-control">
-                </section>
-                <section class="form-group">
-                    <label for="image">Image</label>
-                    <input type="file" name="image" id="image" class="form-control">
-                </section>
-                <section class="form-group">
-                    <label for="comment">Comment</label>
-                    <textarea type="text" name="comment" id="comment" class="form-control"></textarea>
-                </section>
-                <section class="form-group">
+            </div>
+            {!! Form::open(['route' => 'slider.store', 'method' => 'post']) !!}
 
-                    <input type="submit"   value="Register" class="btn btn-success btn-block">
-                </section>
+            {!! Form::close() !!}
 
-            </form>
         </section>
     </section>
 @endsection
 
-@section('css')
-    <style>
-        input[type=text]{
-            border: 2px inset lightgray !important;
-        }
-        textarea{
-            border: 2px inset lightgray  !important;
-            height: 150px !important;
-            resize: none;
-
-        }
-    </style>
-    @endsection
