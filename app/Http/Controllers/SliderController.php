b82d69f208ecc0c700d885172c9e6fdf98b414cf
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-08-16 13:53:25 +0430

    return content with response() and use cookie()

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -12,7 +12,8 @@ class SliderController extends Controller
     public function index()
     {
 
-        return response("This is a content" , 200 )->header("Pdata" , "this is a value of header data");
+        return response("This is a content" , 200 )->cookie("userName" , "Hadi_cookie");
+
 
     }
 
